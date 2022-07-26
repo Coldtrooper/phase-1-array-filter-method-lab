b5f6ef8a325def1be5df4d83bfa28735bef7bd5b
Sylvester <sylvestertrevi@gmail.com>
2022-07-25 20:01:10 -0400

    commit tree 121638d9c0ae82ac5e002e2d6ee9d1574a6c9ed3

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(drivers, driver){
+    return drivers.filter(possibleMatch => possibleMatch.toUpperCase() === driver.toUpperCase())
+} 
+function fuzzyMatch(drivers, namesWithSa){
+    return drivers.filter(matchWithSa => matchWithSa[0,1] === namesWithSa[0,1])
+}
+let drivers = ['Bobby', 'Sammy', 'Stephanie','Sally', 'Annette', 'Sarah', 'bobby'];
+let namesWithSa = "Sa"
+console.log(fuzzyMatch(drivers, namesWithSa))
+
+function matchName(drivers, driver){
+    return drivers.filter(driverMatch => driverMatch.name === driver)
+}
+
+
